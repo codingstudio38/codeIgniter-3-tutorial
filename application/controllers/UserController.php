3f6f5a212433a899a4722dfc7bba75a02fb80d47
v <mondalbdyut@gmail.com>
2023-03-26 16:21:01 +0530

    third commit

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,21 +1,112 @@
  <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . "libraries/vendor/autoload.php";
 date_default_timezone_set("Asia/Kolkata");
 class UserController extends CI_Controller {
- 
+	public $google_client;
+	public $facebook;
+	public $facebook_helper;
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('UserModel');
 		$this->load->library('form_validation');
 		$this->load->library('upload');
+		$this->google_client = new \Google_Client();
+        $this->google_client->setClientId("405380673874-cbp1ep7r2otbtrt75cu4chs4ug9hk6e4.apps.googleusercontent.com");
+        $this->google_client->setClientSecret("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");
+		$this->google_client->setRedirectUri(base_url('/') . 'login-with-google');
+        $this->google_client->addScope('email');
+        $this->google_client->addScope('profile');
+		$this->facebook = new \Facebook\Facebook( array(
+                "app_id" => "1589430858170618",
+                "app_secret" => "d621b2eb8bbc8d0da6567bbc1a560398",
+                "default_graph_version" => "v2.3"
+            )
+        );
+        $this->facebook_helper = $this->facebook->getRedirectLoginHelper();
 	}
 
     public function index()
 	{ 
 		UserNotLoggedIn();
-		$this->load->view('login');
+		//1- app\Libraries -> composer init
+        //2- composer require google/apiclient:^2.12.1
+        // facebook
+        //1- composer require facebook/graph-sdk
+		
+		$this->session->set_userdata('access_token',"");
+		$this->session->set_userdata('fb_access_token',"");
+		unset($_SESSION['access_token']);
+		unset($_SESSION['fb_access_token']);
+		$loginbutton = $this->google_client->createAuthUrl();
+		$fbloginbutton = $this->facebook_helper->getLoginUrl(base_url('/') . 'login-with-facebook', array("email"));
+		$data = array("loginbutton"=>$loginbutton,"fbloginbutton"=>$fbloginbutton);
+		$this->load->view('login',$data);
 	}
  
+
+	
+
+	public function loginwithgoogle()
+	{
+		if (isset($_GET['code']) && $_GET['code']!=="") {
+			$code = $_GET['code'];
+			$token = $this->google_client->fetchAccessTokenWithAuthCode($code);
+			if (!isset($token['error'])) {
+				$this->google_client->setAccessToken($token['access_token']);
+				$this->session->set_userdata('access_token',$token['access_token']);
+				$google_service = new \Google_Service_Oauth2($this->google_client);
+				$googleuser = $google_service->userinfo->get();
+				echo "<pre>";
+				print_r($googleuser);
+				echo "</pre>";
+			}
+		} else {
+			$this->session->set_flashdata('failed',"Code not found. Try again.");
+			redirect(base_url('/'));
+		}
+	}
+
+
+
+	
+	public function loginwithfacebook()
+	{
+		  if (isset($_GET['state']) && $_GET['state']!=="") {
+               $this->facebook_helper->getPersistentDataHandler()->set("state", $_GET['state']);
+            }
+   
+            if (isset($_GET['code']) && $_GET['code']!=="") {
+                $code = $_GET['code'];
+                if (isset($_SESSION['fb_access_token'])) {
+                    $fb_access_token = $this->session->userdata('fb_access_token');
+                } else {
+                    $fb_access_token =$this->facebook_helper->getAccessToken();
+					$this->session->set_userdata('fb_access_token',$fb_access_token);
+                    $this->facebook->setDefaultAccessToken($fb_access_token);
+                }
+                $graph_response = $this->facebook->get('/me?fields=name,email,id,first_name,last_name,link,gender,locale,picture', $fb_access_token);
+                $facebook_user = $graph_response->getGraphUser();
+                if (isset($facebook_user['id'])) {
+                    echo "<pre>";
+                    print_r($facebook_user);
+                    echo "</pre>";
+                }
+
+            } else {
+				$this->session->set_flashdata('failed',"Code not found. Try again.");
+				redirect(base_url('/'));
+			}
+	}
+
+	
+
+
+	
+
+
+
+	
 
     public function register()
 	{ 
--- application/views/login.php
+++ application/views/login.php
@@ -10,15 +10,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>CodeIgniter 3 || Login Page</title>
     <?php include('css_js.php'); ?>
     <style>
-        .invalid{
-            color: red;
-            font-size: small;
-            margin-bottom: 0px;
-        }
+    .invalid {
+        color: red;
+        font-size: small;
+        margin-bottom: 0px;
+    }
     </style>
 </head>
- 
-<body> 
+
+<body>
     <?php include('header.php'); ?>
     <section class="vh-100" style="background-color: #eee;">
         <div class="container h-100">
@@ -30,52 +30,70 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
                                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign In</p>
-                                
-<form class="mx-1 mx-md-4" action="<?= base_url('/userlogin')?>" method="post">
-<?php if(!empty($this->session->flashdata('success'))){?>
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Status</strong> <?=$this->session->flashdata('success');?>
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-<?php } ?>
-<?php if(!empty($this->session->flashdata('failed'))){?>
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Status</strong> <?=$this->session->flashdata('failed');?>
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-<?php } ?>
 
-<div class="d-flex flex-row align-items-center mb-1">
- 
-    <div class="form-outline flex-fill mb-0">
-    <label class="form-label" for="email">Your Email</label>
-        <input type="text" id="email" name="email" class="form-control" value="<?= set_value('email'); ?>"/>
-        <?= form_error('email')?>
-    </div>
-</div>
+                                    <form class="mx-1 mx-md-4" action="<?= base_url('/userlogin')?>" method="post">
+                                        <?php if(!empty($this->session->flashdata('success'))){?>
+                                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                            <strong>Status</strong> <?=$this->session->flashdata('success');?>
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <?php } ?>
+                                        <?php if(!empty($this->session->flashdata('failed'))){?>
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            <strong>Status</strong> <?=$this->session->flashdata('failed');?>
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <?php } ?>
 
+                                        <div class="d-flex flex-row align-items-center mb-1">
 
-<div class="d-flex flex-row align-items-center mb-1">
-    
-    <div class="form-outline flex-fill mb-0">
-    <label class="form-label" for="password">Password</label>
-        <input type="password" id="password" name="password" class="form-control" value="<?= set_value('password'); ?>"/>
-        <?= form_error('password')?>
-    </div>
-</div>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <label class="form-label" for="email">Your Email</label>
+                                                <input type="text" id="email" name="email" class="form-control"
+                                                    value="<?= set_value('email'); ?>" />
+                                                <?= form_error('email')?>
+                                            </div>
+                                        </div>
 
 
+                                        <div class="d-flex flex-row align-items-center mb-1">
+
+                                            <div class="form-outline flex-fill mb-0">
+                                                <label class="form-label" for="password">Password</label>
+                                                <input type="password" id="password" name="password"
+                                                    class="form-control" value="<?= set_value('password'); ?>" />
+                                                <?= form_error('password')?>
+                                            </div>
+                                        </div>
 
 
-<div class="d-flex justify-content-center mx-4 mb-1 mb-lg-4">
-    <button type="submit" class="btn btn-primary">Sing In</button>
-</div>
 
-</form>
+
+                                        <div class="d-flex flex-row align-items-center mb-2">
+                                            <button type="submit" class="btn btn-primary btn-block">Sing In</button>
+
+                                        </div>
+                                        <div style=" text-align: center; margin-top: 5px; ">
+                                            <p>OR</p>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-2">
+                                            <a href="<?= $loginbutton; ?>"
+                                                style=" background-color: #ed430c !important; border-color: #ff0000 !important; margin-top:-5px; color:white !important;width: -webkit-fill-available;"
+                                                class="btn btn-danger"><i style="margin-right:5px;" class="fa fa-google"
+                                                    aria-hidden="true"></i> Login with Google</a>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center mb-1">
+                                            <a href="<?= $fbloginbutton; ?>"
+                                                style="background-color: #665dd8  !important; border-color: #665dd8  !important; color:white !important;width: -webkit-fill-available;"
+                                                class="btn btn-danger"><i style="margin-right:5px;"
+                                                    class="fa fa-facebook" aria-hidden="true"></i> Login with
+                                                Facebook</a>
+                                        </div>
+                                    </form>
 
                                 </div>
                                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
